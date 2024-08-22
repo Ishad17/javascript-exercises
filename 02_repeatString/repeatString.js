7bba7d45c3b0bccba00440f16f3b0d4ea319ddf4
ishad17 <ishadh99@gmail.com>
2024-08-22 19:23:53 +0530

    02 activity done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
+    if (num<0){
+        return 'ERROR';
+    }
+    let word = '';
+
+    for (i=0; i<num; i++){
+        word = word + string;
+    }
+    return word;
 };
+
+repeatString('hey',3);
 
 // Do not edit below this line
 module.exports = repeatString;
