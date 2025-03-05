0599a4c316ade7e653f4baafb81b3f9ade5d64de
ishad ramzan <ishadh99@gmail.com>
2025-03-05 22:32:19 +0530

    Activity 04 Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...elem) {
+
+    
+        for(i = 0; i < elem.length; i++)
+            {
+
+                let index = arr.indexOf(elem[i])
+                if( index != -1){
+
+                    do{
+                        console.log(index)
+                        arr.splice(index,1);
+                        index = arr.indexOf(elem[i])
+                    }
+                     while(index != -1)
+                }
+
+
+            }
+
+        return arr;    
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
